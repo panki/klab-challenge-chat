72ff9eb730a2cdde3861e0458f862df9681be414
Alexander Panko <god@panki.ru>
2016-06-21 14:31:38 +0200

    Add graceful shutdown

--- app/main.js
+++ app/main.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import logger from 'morgan'
 import path from 'path'
+import httpShutdown from 'http-shutdown'
 
 import routes from './routes'
 import websocketServer from './websocket/server'
@@ -17,11 +18,35 @@ app.use('/public', express.static(path.join(__dirname, 'public')))
 app.use(routes)
 
 // Start server
-const server = app.listen(3000, () => {
+const server = httpShutdown(app.listen(3000, () => {
   var host = server.address().address
   var port = server.address().port
   console.log('Web server is listening on %s:%s', host, port)
-})
+}))
 
 // Start websocket server
-websocketServer(server)
+const ws = websocketServer(server)
+
+// Graceful shutdown
+function shutdown (reason, exitCode = 0) {
+  console.log(`Terminate on ${reason}`)
+  console.log('Cleaning up...')
+  console.log('Shutdown websocket server...')
+  ws.closeAllConnections()
+  console.log('Shutdown web server...')
+  server.shutdown(() => {
+    console.log('Closed out remaining http connections')
+    process.nextTick(() => process.exit(exitCode))
+    console.log(`Exiting with code ${exitCode}`)
+  })
+}
+
+process.once('SIGINT', () => shutdown('Ctrl-C'))
+process.once('SIGTERM', () => shutdown('SIGTERM'))
+process.once('SIGHUP', () => shutdown('SIGHUP'))
+process.once('uncaughtException', (e) => {
+  console.log('Uncaught Exception:')
+  console.log(e.stack)
+  shutdown('Exception', 99)
+})
+
